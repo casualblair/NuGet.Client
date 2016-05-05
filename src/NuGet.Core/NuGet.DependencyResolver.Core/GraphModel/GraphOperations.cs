01a6d7dfbcdf4395163c2520941688d9d26c5554
Joel Verhagen <jver@microsoft.com>
2016-05-05 13:09:11 -0700

    Add more tracing

--- src/NuGet.Core/NuGet.DependencyResolver.Core/GraphModel/GraphOperations.cs
+++ src/NuGet.Core/NuGet.DependencyResolver.Core/GraphModel/GraphOperations.cs
@@ -143,7 +143,12 @@ namespace NuGet.DependencyResolver
 
         private static string PrettyPrint<TItem>(this GraphNode<TItem> node)
         {
-            return node.Key.Name + " " + node.Key.VersionRange?.ToNonSnapshotRange().PrettyPrint();
+            return
+                node.Key.Name +
+                " (" +
+                LibraryDependencyTargetUtils.GetFlagString(node.Key.TypeConstraint) +
+                ") "
+                + node.Key.VersionRange?.ToNonSnapshotRange().PrettyPrint();
         }
 
         private static bool TryResolveConflicts<TItem>(this GraphNode<TItem> root, List<VersionConflictResult<TItem>> versionConflicts)
